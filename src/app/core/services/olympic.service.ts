8a99896c610dadc7e536147988888f8008646b68
RyShad0wz <allan.bouhadjar@gmail.com>
2025-02-13 00:19:25 +0100

    Create new models and service

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -2,26 +2,27 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, EMPTY } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import { OlympicCountry } from '../models/Olympic';
+import { Participation } from '../models/Participation';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
+
   // Utilisation du fichier JSON local
   private jsonDataUrl = '/assets/data/olympics.json'; 
-  
-  // API utilisable vers les données des JO 2024
-  // private apiUrl = 'https://data.education.gouv.fr/api/explore/v2.1/catalog/datasets/paris-2024-results-medals-oly-eng/records?limit=20';
 
-  private olympics$ = new BehaviorSubject<any[] | null>(null);
+  private olympics$ = new BehaviorSubject<OlympicCountry[] | null>(null);
 
   constructor(private http: HttpClient) {}
 
   /**
    * Charge les données des JO (depuis un fichier JSON)
    */
-  loadInitialData(): Observable<any[]> {
-    return this.http.get<any[]>(this.jsonDataUrl).pipe(
+
+  loadInitialData(): Observable<OlympicCountry[]> {
+    return this.http.get<OlympicCountry[]>(this.jsonDataUrl).pipe(
       tap((data) => {
         console.log('✅ Données chargées depuis le JSON:', data);
         this.olympics$.next(data);
@@ -35,8 +36,22 @@ export class OlympicService {
   }
 
   /**
+   * Récupère les données des JO sous forme d'observable
+   */
+
+    getOlympics(): Observable<any[] | null> {
+      return this.olympics$.asObservable();
+    }
+  }
+
+
+  // API utilisable vers les données des JO 2024
+  // private apiUrl = 'https://data.education.gouv.fr/api/explore/v2.1/catalog/datasets/paris-2024-results-medals-oly-eng/records?limit=20';
+
+  /**
    * Méthode alternative pour récupérer les données via une API
    */
+
   /*
   loadInitialDataFromAPI(): Observable<any[]> {
     return this.http.get<any[]>(this.apiUrl).pipe(
@@ -53,10 +68,4 @@ export class OlympicService {
   }
   */
 
-  /**
-   * Récupère les données des JO sous forme d'observable
-   */
-  getOlympics(): Observable<any[] | null> {
-    return this.olympics$.asObservable();
-  }
-}
+
